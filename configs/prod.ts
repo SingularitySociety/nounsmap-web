a87ccaecf82affb8bf62cadd46c48624a6d6c8b0
isamu <isamu@to-kyo.to>
2022-04-29 09:29:36 +0900

    add prod config and update deploy script

---
+++ configs/prod.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB6Rj9kOnPlKG_aSOQT5_lGDBP01JmR-7U",
+  authDomain: "nounsmap-web-prod.firebaseapp.com",
+  projectId: "nounsmap-web-prod",
+  storageBucket: "nounsmap-web-prod.appspot.com",
+  messagingSenderId: "297116670466",
+  appId: "1:297116670466:web:c00780dd6d818858bae51b",
+  measurementId: "G-2TR3L7VH0H"
+};
