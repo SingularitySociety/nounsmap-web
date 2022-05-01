5caa42198f93a7ff653843260e43ec2be03ab74c
papapapine <zewezpd@gmail.com>
2022-05-02 00:14:28 +0900

    display Ukraine Map

---
+++ src/components/NounsMap.vue
@@ -0,0 +1,35 @@
+<template>
+    <div ref="map" class="nouns-map"></div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      map:'',
+    }
+  },
+  mounted(){
+    let timer = setInterval(() => {
+      if(window.google){
+        clearInterval(timer);
+        this.map = new window.google.maps.Map(this.$refs.map, {
+          // lat 緯度 lng 経度
+          center: {lat: 49, lng: 31.5},
+          zoom: 7
+        });       
+      }
+    },500)
+  }
+}
+
+</script>
+<style>
+
+/* マップを画面幅に合わせる*/
+.nouns-map {
+    width: 100vw;
+    height: 100vh;
+}
+
+</style>
