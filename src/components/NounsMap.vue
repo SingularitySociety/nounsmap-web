27cfbd282cdea56a47c25dec519eb3b3e6c09f1a
isamu <isamu@to-kyo.to>
2022-05-03 19:29:29 +0900

    support googlemaps/js-api-loader and use Composition API

--- src/components/NounsMap.vue
+++ src/components/NounsMap.vue
@@ -1,35 +1,38 @@
 <template>
-    <div ref="map" class="nouns-map"></div>
+  <div ref="mapRef" class="nouns-map" />
 </template>
 
-<script>
-export default {
-  data(){
-    return {
-      map:'',
-    }
-  },
-  mounted(){
-    let timer = setInterval(() => {
-      if(window.google){
-        clearInterval(timer);
-        this.map = new window.google.maps.Map(this.$refs.map, {
-          // lat 緯度 lng 経度
-          center: {lat: 49, lng: 34.5},
-          zoom: 5.5
-        });       
-      }
-    },500)
-  }
-}
+<script lang="ts">
+import { defineComponent, ref, onMounted } from "vue";
+import { Loader } from "@googlemaps/js-api-loader";
 
+export default defineComponent({
+  setup() {
+    const mapRef = ref();
+    onMounted(async () => {
+      const loader = new Loader({
+        apiKey: "AIzaSyC-sE86tDfCgxPjsx1heo2iwvDRgmOYsFo",
+        libraries: ["places"],
+      });
+
+      const mapOptions = {
+        // lat 緯度 lng 経度
+        center: { lat: 49, lng: 34.5 },
+        zoom: 5.5,
+      };
+      const google = await loader.load();
+      new google.maps.Map(mapRef.value, mapOptions);
+    });
+    return {
+      mapRef,
+    };
+  },
+});
 </script>
 <style>
-
 /* マップを画面幅に合わせる*/
 .nouns-map {
-    width: 100vw;
-    height: 100vh;
+  width: 100vw;
+  height: 100vh;
 }
-
 </style>
