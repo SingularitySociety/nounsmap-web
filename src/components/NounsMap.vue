624cdc08b46742f06988e96b49cb424a06edf7de
isamu <isamu@to-kyo.to>
2022-05-06 15:17:25 +0900

    add Heatmap example

--- src/components/NounsMap.vue
+++ src/components/NounsMap.vue
@@ -9,11 +9,14 @@ import { Loader } from "@googlemaps/js-api-loader";
 export default defineComponent({
   setup() {
     const mapRef = ref();
+
     const google = ref();
+    const mapObj = ref();
+    
     onMounted(async () => {
       const loader = new Loader({
         apiKey: "AIzaSyC-sE86tDfCgxPjsx1heo2iwvDRgmOYsFo",
-        libraries: ["places"],
+        libraries: ["places", "visualization"],
       });
 
       const mapOptions = {
@@ -22,8 +25,31 @@ export default defineComponent({
         zoom: 5.5,
       };
       google.value = await loader.load();
-      new google.value.maps.Map(mapRef.value, mapOptions);
+      mapObj.value = new google.value.maps.Map(mapRef.value, mapOptions);
+
+      const heatmapPoints = [
+        {
+          location: new google.value.maps.LatLng(49, 34.5),
+          weight: 1,
+        },
+        {
+          location: new google.value.maps.LatLng(49.3, 34.3),
+          weight: 1,
+        },
+        {
+          location: new google.value.maps.LatLng(49.5, 34.4),
+          weight: 2,
+        },
+      ]
+      
+      const heatmap = new google.value.maps.visualization.HeatmapLayer({
+        data: heatmapPoints,
+        map: mapObj.value
+      });
+      heatmap.setMap(mapObj.value);
     });
+
+
     return {
       mapRef,
     };
