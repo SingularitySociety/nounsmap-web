df6a933d2e6bada705a90676e5bfb53266ffc7fe
TAK_Ryuji <kozayupapa@gmail.com>
2024-01-07 09:04:51 +0900

    remove rinkeby contract

--- configs/prod.ts
+++ configs/prod.ts
@@ -57,14 +57,6 @@ export const ethereumConfig = {
       idmask: 0,
     },
     {
-      name: "NounsLove(testnets)",
-      chainId: "0x4",
-      contractAddress: "0x1602155eB091F863e7e776a83e1c330c828ede19",
-      openseaUrl: "https://testnets.opensea.io/assets/rinkeby",
-      filter: null,
-      idmask: 0,
-    },
-    {
       name: "NamedNoun(polygon)",
       chainId: "0x89",
       contractAddress: "0x2953399124F0cBB46d2CbACD8A89cF0599974963",
